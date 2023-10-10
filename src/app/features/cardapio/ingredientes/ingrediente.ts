70a166e34c602b77d68b5c618415e2c63424966a
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-10-10 19:36:35 -0300

    Models de Ingrediente e sabores

---
+++ src/app/features/cardapio/ingredientes/ingrediente.ts
@@ -0,0 +1,20 @@
+import { AbstractEntity } from "src/app/shared/models/abstract-entity";
+import { Sabores } from "../sabores/sabor";
+
+export class ingredientes extends AbstractEntity{
+
+    nomeIngrediente: string;
+    qtdeIngrediente: number;
+    sabores: Sabores[];
+
+    constructor(nomeIngrediente: string, qtdeIngrediente: number, sabores: Sabores[]){
+
+        super();
+
+        this.nomeIngrediente = nomeIngrediente;
+        this.qtdeIngrediente = qtdeIngrediente;
+        this.sabores = sabores;
+
+    }
+
+}
