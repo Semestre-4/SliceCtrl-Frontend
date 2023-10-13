a6e65c1c3546306dab38722e13401e7990b87d28
Bouchra Akl <bushraakl1234@gmail.com>
2023-10-13 19:09:24 -0300

    sidebar responsiveness

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { ElementRef, Renderer2 } from '@angular/core';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +8,22 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  toggleSidebar() {
+    const sidebar = this.el.nativeElement.querySelector('.sidebar');
+    const content = this.el.nativeElement.querySelector('.content');
+
+    if (sidebar.classList.contains('collapsed-sidebar')) {
+      this.renderer.removeClass(sidebar, 'collapsed-sidebar');
+      this.renderer.removeClass(content, 'expanded-content');
+    } else {
+      this.renderer.addClass(sidebar, 'collapsed-sidebar');
+      this.renderer.addClass(content, 'expanded-content');
+    }
+  }
+
+
 
 }
