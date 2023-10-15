a18c59e73fe636e50b38478462c2c51b243c6658
Bouchra Akl <bushraakl1234@gmail.com>
2023-10-15 16:49:57 -0300

    feat : products display functioning.

--- src/app/features/pedidos/menu-pedido/menu-pedido.component.ts
+++ src/app/features/pedidos/menu-pedido/menu-pedido.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { ProdutosService } from '../../cardapio/produtos/service/produtos.service';
 import { PizzasService } from '../../cardapio/pizzas/service/pizzas.service';
 import { forkJoin } from 'rxjs';
+import { Categoria } from 'src/app/shared/models/enums/categoria';
 
 @Component({
   selector: 'app-menu-pedido',
@@ -12,6 +13,7 @@ export class MenuPedidoComponent implements OnInit {
   @Input() options: string[] = [];
   products: any[] = [];
   filteredProducts: any[] = [];
+  selectedCategory: string = 'Todos';
 
   constructor(
     private productService: ProdutosService,
@@ -42,12 +44,23 @@ export class MenuPedidoComponent implements OnInit {
   }
 
   onCategorySelected(category: string): void {
-    if (category === 'Todos') {
+    this.selectedCategory = category;
+    this.filterProducts();
+  }
+
+  filterProducts(): void {
+    if (this.selectedCategory === 'Todos') {
       this.filteredProducts = this.products;
-    } else if (category === 'Pizza') {
-      this.filteredProducts = this.products.filter(product => product.isPizza);
-    } else {
-      this.filteredProducts = this.products.filter(product => product.category === category);
+    } else if (this.selectedCategory === 'Bebidas') {
+      this.filteredProducts = this.products.filter(product => product.categoria === Categoria.BEBIDAS);
+    } else if (this.selectedCategory === 'Acompanhamentos') {
+      this.filteredProducts = this.products.filter(product => product.categoria === Categoria.ACOMPANHAMENTOS);
+    } else if (this.selectedCategory === 'Sobremesas') {
+      this.filteredProducts = this.products.filter(product => product.categoria === Categoria.SOBREMESAS);
+    } else if (this.selectedCategory === 'Outros') {
+      this.filteredProducts = this.products.filter(product => product.categoria === Categoria.OUTROS);
+    }else{
+      this.filteredProducts = this.products.filter(product => product.isPizza === true);
     }
   }
 }
