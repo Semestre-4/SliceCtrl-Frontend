8891276e23c6d86a2118dac13be477a20d35fef8
Bouchra Akl <bushraakl1234@gmail.com>
2023-10-18 19:40:20 -0300

    table component creatation and bug fixes

--- src/app/features/pedidos/listar-pedido/listar-pedido.component.ts
+++ src/app/features/pedidos/listar-pedido/listar-pedido.component.ts
@@ -6,6 +6,7 @@ import { Cliente } from '../../clientes/cliente';
 import { Funcionario } from '../../funcionarios/funcionario';
 import { Pagamento } from '../models/pagamento';
 import { FormaDeEntrega } from 'src/app/shared/models/enums/forma-entrega';
+import { TableHeader } from 'src/app/shared/components/table/table-header';
 
 @Component({
   selector: 'app-listar-pedido',
@@ -14,7 +15,7 @@ import { FormaDeEntrega } from 'src/app/shared/models/enums/forma-entrega';
 })
 export class ListarPedidoComponent implements OnInit{
   data: any[] = [];
-  tableHeaders: string[] = [];
+
 
   p: Pedido = new Pedido(
     new Cliente('', '', '', '', [], []),
@@ -32,24 +33,37 @@ export class ListarPedidoComponent implements OnInit{
   constructor(private pedidoService: PedidoService) { }
 
   ngOnInit(): void {
-    this.pedidoService.getAllPedidos().subscribe((response: any[]) => {
-      this.data = response;
-    });
-  }
-
-  getStatusClass(status: string): string {
-    switch(status) {
-      case 'PENDENTE':
-        return 'badge-yellow'; 
-      case 'PAGO':
-        return 'badge-green';
-      case 'CANCELADO':
-        return 'badge-red';
-      case 'TODOS':
-        return 'badge-danger';
-      default:
-        return '';
-    }
+    this.pedidoService.getAllPedidos().subscribe(
+      (response: any[]) => {
+        this.data = response;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
   }
   
+
+  onButtonClick(action: string) {
+    console.log(action);
+  }
+
+  apiUrlPath(){
+    return 'http://localhost:8080/api/pedido/all';  
+  }
+
+  callHeaders(){
+    let tableHeaders : TableHeader[] = [];
+    tableHeaders.push(new TableHeader('Cliente', 'cliente.nome'));
+    tableHeaders.push(new TableHeader('Funcionario', 'funcionario.nome'));
+    tableHeaders.push(new TableHeader('Data','cadastro'));
+    tableHeaders.push(new TableHeader('Forma de Retirada','formaDeEntrega'));
+    tableHeaders.push(new TableHeader('Status','status'));
+    tableHeaders.push(new TableHeader('ValorTotal','valorTotal'));
+    return tableHeaders;
+  }
+  
+  handleButtonClick(event: string) {
+    console.log(`Button clicked: ${event}`);
+  }
 }
--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,10 +1,64 @@
-import { Component, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { inject } from '@angular/core/testing';
+import { Pedido } from 'src/app/features/pedidos/models/pedido';
+import { FormatarDataPipe } from '../../pipes/formatar-data/formatar-data.pipe';
+import { FormatarPrecoPipe } from '../../pipes/formatar-preco/formatar-preco.pipe';
+import { TableHeader } from './table-header';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent {
+export class TableComponent implements OnInit {
+  @Input() headers: TableHeader[] = [];
+  @Input() apiUrl: string = '';
+  @Input() actions: any[] = [];
+  data: any[] = [];
+  @Output() buttonClick = new EventEmitter<string>();
 
+  carregando: boolean = true;
+
+  constructor(private http: HttpClient,
+    private datePipe: FormatarDataPipe,
+    private numberPipe: FormatarPrecoPipe,
+    ) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData() {
+    this.http.get<any[]>(this.apiUrl).subscribe(data => {
+      this.data = data;
+      console.log(this.data);
+      this.carregando = false;
+    });
+  }
+
+  retornarValor(tableHeader:TableHeader, item: any) {
+    const parte = tableHeader.campo.split('.');
+    let valor = item;
+    for (const p of parte) {
+      if(valor != null){
+        valor = valor[p];
+      }
+    }
+    try {
+      let formatedDate = new Date(valor);
+      console.log(formatedDate)
+      if(formatedDate.toString() != "Invalid Date" && formatedDate.toString() != "Wed Dec 31 1969 21:00:00 GMT-0300 (Brasilia Standard Time)"){
+      valor = this.datePipe.transform(valor);
+      }
+      if(formatedDate.toString() == "Wed Dec 31 1969 21:00:00 GMT-0300 (Brasilia Standard Time)"){
+        valor = this.numberPipe.transform(valor);
+      }
+    } catch (error) {
+      
+    }
+    return valor;
+  }
 }
+
+
