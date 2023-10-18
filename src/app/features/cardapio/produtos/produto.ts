420653a2504681b82809d227ae592c099923eb84
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-10-17 21:21:00 -0300

    Finalizado layout de registrar produto

--- src/app/features/cardapio/produtos/produto.ts
+++ src/app/features/cardapio/produtos/produto.ts
@@ -4,24 +4,11 @@ import { Pedido } from "../../pedidos/models/pedido";
 
 export class Produtos extends AbstractEntity {
     
-    nomeProduto: string;
-    categoria: Categoria;
-    qtdeEstoque: number;
-    preco: number;
-    disponivel: boolean;
-    pedidos: Pedido[];
-
-    constructor(nomeProduto: string, categoria: Categoria, qtdeEstoque: number, preco: number, disponivel: boolean, pedidos: Pedido[]){
-
-        super();
-
-        this.nomeProduto = nomeProduto;
-        this.categoria = categoria;
-        this.qtdeEstoque = qtdeEstoque;
-        this.preco = preco;
-        this.disponivel = disponivel;
-        this.pedidos = pedidos;
-
-    }
+    nomeProduto!: string;
+    categoria!: Categoria;
+    qtdeEstoque!: number;
+    preco!: number;
+    disponivel!: boolean;
+    pedidos!: Pedido[];
 
 }
--- src/app/features/cardapio/produtos/registrar-produtos/registrar-produtos.component.ts
+++ src/app/features/cardapio/produtos/registrar-produtos/registrar-produtos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Categoria } from 'src/app/shared/models/enums/categoria';
+import { Produtos } from '../produto';
 
 @Component({
   selector: 'app-registrar-produtos',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./registrar-produtos.component.scss']
 })
 export class RegistrarProdutosComponent {
+
+  categoria!: Categoria;
+  produto: Produtos = new Produtos();
+
+  categoriaOption = Object.values(Categoria);
 
 }
