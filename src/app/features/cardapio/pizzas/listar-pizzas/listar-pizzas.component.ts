5af2735caf32475ab180db6d58ddc8df2240c60e
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-10-19 19:27:38 -0300

    Listar pizza implementado (Corrigir bugs)

--- src/app/features/cardapio/pizzas/listar-pizzas/listar-pizzas.component.ts
+++ src/app/features/cardapio/pizzas/listar-pizzas/listar-pizzas.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PizzasService } from '../service/pizzas.service';
+import { TableHeader } from 'src/app/shared/components/table/table-header';
 
 @Component({
   selector: 'app-listar-pizzas',
   templateUrl: './listar-pizzas.component.html',
   styleUrls: ['./listar-pizzas.component.scss']
 })
-export class ListarPizzasComponent {
+export class ListarPizzasComponent implements OnInit{
+  data: any[] = [];
 
+  constructor(private service: PizzasService) { }
+
+  ngOnInit(): void {
+    this.service.getAll().subscribe(
+      (response: any[]) => {
+        this.data = response;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
+  
+
+  apiUrlPath(){
+    return 'http://localhost:8080/api/pizza/all';  
+  }
+
+  callHeaders(){
+    let tableHeaders : TableHeader[] = [];
+    tableHeaders.push(new TableHeader('', ''));
+    tableHeaders.push(new TableHeader('', ''));
+    tableHeaders.push(new TableHeader('Tamanho', 'tamanho'));
+    tableHeaders.push(new TableHeader('', ''));
+    tableHeaders.push(new TableHeader('Preço', 'preco'));
+    tableHeaders.push(new TableHeader('', ''));
+
+    console.log(this.data[0].nomeProduto)
+
+    return tableHeaders;
+  }
 }
