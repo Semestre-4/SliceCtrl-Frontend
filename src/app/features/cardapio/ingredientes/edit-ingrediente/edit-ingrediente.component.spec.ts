690d0b4acee1bc30c83e01667371f3a7f37bdb91
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-11-26 20:45:53 -0300

    Test ingredientes inputs ok

--- src/app/features/cardapio/ingredientes/edit-ingrediente/edit-ingrediente.component.spec.ts
+++ src/app/features/cardapio/ingredientes/edit-ingrediente/edit-ingrediente.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { EditIngredienteComponent } from './edit-ingrediente.component';
+import { By } from '@angular/platform-browser';
+import { Ingredientes } from '../ingrediente';
 
 describe('EditIngredienteComponent', () => {
   let component: EditIngredienteComponent;
@@ -8,7 +11,13 @@ describe('EditIngredienteComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EditIngredienteComponent]
+      declarations: [EditIngredienteComponent],
+      imports: [
+        HttpClientTestingModule
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA
+      ]
     });
     fixture = TestBed.createComponent(EditIngredienteComponent);
     component = fixture.componentInstance;
@@ -17,5 +26,26 @@ describe('EditIngredienteComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+    
+  beforeEach(() => {
+    let ingrediente = new Ingredientes();
+    ingrediente.nomeIngrediente = 'Ingrediente';
+    ingrediente.qtdeIngrediente = 5;
+    
+    component.ingrediente = ingrediente;
+    fixture.detectChanges();
+  });
+
+
+  it('Teste input ingrediente nome.', () => {
+    let elemento = fixture.debugElement.query(By.css('input[name="nomeIngrediente"]'));
+    expect(elemento.nativeElement.ngModel).toEqual('Ingrediente');
+  });
+
+  it('Teste input qtdeIngrediente.', () => {
+    let elemento = fixture.debugElement.query(By.css('input[name="qtdeIngrediente"]'));
+    expect(elemento.nativeElement.ngModel).toEqual(5);
   });
 });
--- src/app/features/cardapio/ingredientes/registrar-ingredientes/registrar-ingredientes.component.spec.ts
+++ src/app/features/cardapio/ingredientes/registrar-ingredientes/registrar-ingredientes.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RegistrarIngredientesComponent } from './registrar-ingredientes.component';
+import { Ingredientes } from '../ingrediente';
+import { Sabores } from '../../sabores/sabor';
+import { By } from '@angular/platform-browser';
 
 describe('RegistrarIngredientesComponent', () => {
   let component: RegistrarIngredientesComponent;
@@ -8,7 +12,13 @@ describe('RegistrarIngredientesComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [RegistrarIngredientesComponent]
+      declarations: [RegistrarIngredientesComponent],
+      imports: [
+        HttpClientTestingModule
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA
+      ]
     });
     fixture = TestBed.createComponent(RegistrarIngredientesComponent);
     component = fixture.componentInstance;
@@ -18,4 +28,26 @@ describe('RegistrarIngredientesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  
+  beforeEach(() => {
+    let ingrediente = new Ingredientes();
+    ingrediente.nomeIngrediente = 'Ingrediente';
+    ingrediente.qtdeIngrediente = 5;
+    
+    component.ingrediente = ingrediente;
+    fixture.detectChanges();
+  });
+
+
+  it('Teste input ingrediente nome.', () => {
+    let elemento = fixture.debugElement.query(By.css('input[name="nomeIngrediente"]'));
+    expect(elemento.nativeElement.ngModel).toEqual('Ingrediente');
+  });
+
+  it('Teste input qtdeIngrediente.', () => {
+    let elemento = fixture.debugElement.query(By.css('input[name="qtdeIngrediente"]'));
+    expect(elemento.nativeElement.ngModel).toEqual(5);
+  });
+
 });
