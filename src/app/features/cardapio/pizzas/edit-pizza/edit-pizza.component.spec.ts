59e6d2b62f2bf1dde660465b935c981171e4bfde
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-11-26 20:38:47 -0300

    pizza test

--- src/app/features/cardapio/pizzas/edit-pizza/edit-pizza.component.spec.ts
+++ src/app/features/cardapio/pizzas/edit-pizza/edit-pizza.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { EditPizzaComponent } from './edit-pizza.component';
+import { Pizzas } from '../pizza';
+import { Tamanho } from 'src/app/shared/models/enums/tamanho-pizza';
+import { By } from '@angular/platform-browser';
 
 describe('EditPizzaComponent', () => {
   let component: EditPizzaComponent;
@@ -8,7 +12,13 @@ describe('EditPizzaComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EditPizzaComponent]
+      declarations: [EditPizzaComponent],
+      imports: [
+        HttpClientTestingModule
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA
+      ]
     });
     fixture = TestBed.createComponent(EditPizzaComponent);
     component = fixture.componentInstance;
@@ -17,5 +27,25 @@ describe('EditPizzaComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  
+  beforeEach(() => {
+    let pizza = new Pizzas();
+    pizza.preco = 29.90;
+    pizza.tamanho = Tamanho.M;
+    component.pizza = pizza;
+    fixture.detectChanges();
+  });
+
+
+  it('Teste input preço pizza.', () => {
+    let elemento = fixture.debugElement.query(By.css('input[name="precoPizza"]'));
+    expect(elemento.nativeElement.ngModel).toEqual(29.90);
+  });
+
+  it('Teste input tamanho pizza.', () => {
+    let elemento = fixture.debugElement.query(By.css('input[name="tamanhoPizza"]'));
+    expect(elemento.nativeElement.ngModel).toEqual('M');
   });
 });
