8c67cf87a772c7030241ba8f58dea61c294b18c8
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-11-26 20:51:31 -0300

    Test sabor inputs

--- src/app/features/cardapio/sabores/edit-sabor/edit-sabor.component.spec.ts
+++ src/app/features/cardapio/sabores/edit-sabor/edit-sabor.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { EditSaborComponent } from './edit-sabor.component';
+import { By } from '@angular/platform-browser';
+import { Ingredientes } from '../../ingredientes/ingrediente';
+import { Sabores } from '../sabor';
 
 describe('EditSaborComponent', () => {
   let component: EditSaborComponent;
@@ -8,7 +12,13 @@ describe('EditSaborComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EditSaborComponent]
+      declarations: [EditSaborComponent],
+      imports: [
+        HttpClientTestingModule
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA
+      ]
     });
     fixture = TestBed.createComponent(EditSaborComponent);
     component = fixture.componentInstance;
@@ -18,4 +28,53 @@ describe('EditSaborComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  beforeEach(() => {
+    let sabor = new Sabores();
+    let ingrediente = new Ingredientes();
+    let ingredientes!: Ingredientes[];
+    
+    ingrediente.nomeIngrediente = 'Ingrediente';
+    ingrediente.qtdeIngrediente = 5;
+
+    ingredientes.push(ingrediente);
+
+    sabor.nomeSabor ='Sabor';
+    sabor.descricao = 'Descrição'
+    sabor.valorAdicional= 5.50;
+    sabor.ingredientesDTOS = ingredientes;
+    
+    component.sabor = sabor;
+    fixture.detectChanges();
+  });
+
+
+  it('Teste input sabor nome.', () => {
+    let elemento = fixture.debugElement.query(By.css('input[name="nomeSabor"]'));
+    expect(elemento.nativeElement.ngModel).toEqual('Sabor');
+  });
+
+  it('Teste input qtdeIngrediente.', () => {
+    let elemento = fixture.debugElement.query(By.css('input[name="descSabor"]'));
+    expect(elemento.nativeElement.ngModel).toEqual('Descrição');
+  });
+
+  it('Teste input valor adicional sabor.', () => {
+    let elemento = fixture.debugElement.query(By.css('input[name="valorAdicional"]'));
+    expect(elemento.nativeElement.ngModel).toEqual(5.50);
+  });
+
+  it('Teste input sabor ingredientes.', () => {
+    let ingrediente = new Ingredientes();
+    let ingredientes!: Ingredientes[];
+    
+    ingrediente.nomeIngrediente = 'Ingrediente';
+    ingrediente.qtdeIngrediente = 5;
+
+    ingredientes.push(ingrediente);
+
+    let elemento = fixture.debugElement.query(By.css('input[name="ingredientesSabor"]'));
+    expect(elemento.nativeElement.ngModel).toEqual(ingredientes);
+  });
+
 });
