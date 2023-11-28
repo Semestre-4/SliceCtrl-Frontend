cf622ad676e74a456c552f8af29ca89fa112f188
Bouchra Akl <bushraakl1234@gmail.com>
2023-11-28 18:59:40 -0300

    fixes

--- src/app/core/usuario/usuario.module.ts
+++ src/app/core/usuario/usuario.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AbstractEntity } from 'src/app/shared/models/abstract-entity';
-import { ListarPerfilUsuarioComponent } from './perfil-usuario/listar-perfil-usuario/listar-perfil-usuario.component';
 import { RegistrarPerfilUsuarioComponent } from './perfil-usuario/registrar-perfil-usuario/registrar-perfil-usuario.component';
 
 @NgModule({
   declarations: [
-    ListarPerfilUsuarioComponent,
     RegistrarPerfilUsuarioComponent
   ],
   imports: [
