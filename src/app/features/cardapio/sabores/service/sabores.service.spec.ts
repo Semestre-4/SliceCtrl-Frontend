d25b203c3e2cd392b753bd49de4f4b82af3bf3b8
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-11-28 19:39:47 -0300

    test service

--- src/app/features/cardapio/sabores/service/sabores.service.spec.ts
+++ src/app/features/cardapio/sabores/service/sabores.service.spec.ts
@@ -1,14 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { SaboresService } from './sabores.service';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/compiler';
 import { of } from 'rxjs';
+import { UsuarioService } from 'src/app/features/usuarios/service/usuario.service';
+import { Sabores } from '../sabor';
 
 describe('SaboresService', () => {
   let service: SaboresService;
   let http: HttpClient;
+  let saborService: SaboresService;
+  let newSabor = new Sabores();
+
+
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -24,11 +31,25 @@ describe('SaboresService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('test get all', () =>{
-    let resposta = 'Resposta'
-    const spy = spyOn(http, 'get').and.returnValue(of({body: resposta}));
-    service.getAll();
-    expect(spy).toHaveBeenCalledTimes(1);
+  beforeEach(() => {
+    saborService = new SaboresService(http);
+
+    newSabor.ativo = true;
+    newSabor.cadastro = new Date();
+    newSabor.descricao = 'descrição';
+    newSabor.id = 1;
+    newSabor.nomeSabor = 'Sabor';
+
   });
+
+  it('should get a sabor by id', fakeAsync(() => {
+    const createdSabor$ = saborService.getById(1);
+    let createdSabor = new Sabores;
+    createdSabor$.subscribe((sabor) => {
+      createdSabor = sabor;
+    });
+    tick(5000); // Adjust the time based on your expected asynchronous operation time
+    expect(createdSabor).toBeTruthy(); // Check if the created sabor exists
+  }));
 
 });
